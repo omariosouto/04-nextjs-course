0a31188d6aad8ee1de11cfc011ddf8cdfe4fc43c
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-04-06 12:20:01 -0300

    update

--- src/screens/FAQQuestionScreen/index.js
+++ src/screens/FAQQuestionScreen/index.js
@@ -6,6 +6,7 @@ import { cmsService } from '../../infra/cms/cmsService';
 import { renderNodeRule, StructuredText } from 'react-datocms';
 import { isHeading } from 'datocms-structured-text-utils';
 import CMSProvider from '../../infra/cms/CMSProvider';
+import { pageHOC } from '../../components/wrappers/pageHOC';
 
 
 export async function getStaticPaths() {
@@ -46,9 +47,9 @@ export async function getStaticProps({ params, preview }) {
   }
 }
 
-export default function FAQQuestionScreen({ cmsContent }) {
+function FAQQuestionScreen({ cmsContent }) {
   return (
-    <CMSProvider cmsContent={cmsContent}>
+    <>
       <Head>
         <title>FAQ - Alura</title>
       </Head>
@@ -98,6 +99,8 @@ export default function FAQQuestionScreen({ cmsContent }) {
       </Box>
 
       <Footer />
-    </CMSProvider>
+    </>
   )
 }
+
+export default pageHOC(FAQQuestionScreen)
